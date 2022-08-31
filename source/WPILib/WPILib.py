ccf3f8b436c28670d92f623894c87474aa0c2c78
Ansel Chang <31443946+AnselChang@users.noreply.github.com>
2022-08-30 23:48:42 -0400

    add source files to dev

---
+++ source/WPILib/WPILib.py
@@ -0,0 +1,25 @@
+import board as _board
+from . import _drivetrain
+from . import _grove_ultrasonic
+from . import _reflectance
+from . import _servo
+from . import _buttons
+from . import _encoded_motor
+from . import _led
+
+import time
+
+# hidden motor variables
+_leftMotor = _drivetrain._encoded_motor.EncodedMotor(encoderPinA=_board.GP4, encoderPinB=_board.GP5,
+                                               motorPin1=_board.GP8, motorPin2=_board.GP9, doFlip=True)
+
+_rightMotor = _drivetrain._encoded_motor.EncodedMotor(encoderPinA=_board.GP2, encoderPinB=_board.GP3,
+                                               motorPin1=_board.GP10, motorPin2=_board.GP11, doFlip=False)
+
+# Publicly-accessible objects
+drivetrain = _drivetrain.Drivetrain(_leftMotor, _rightMotor, wheel_diameter=6.5, wheel_spacing=16) # units in cm
+reflectance = _reflectance.Reflectance(_board.GP27, _board.GP26)
+sonar = _grove_ultrasonic.GroveUltrasonicRanger(_board.GP28)
+led = _led.RGBLED(_board.GP18)
+servo = _servo.Servo(_board.GP12, actuationRange = 135)
+buttons = _buttons.Buttons()
---
+++ source/code.py
@@ -0,0 +1,14 @@
+from WPILib.WPILib import *
+
+## Optional imports for use with sample code
+from SampleCode.sample_drive_methods import *
+from SampleCode.sample_sensor_access import *
+from SampleCode.sample_miscellaneous import *
+
+def main():
+    wait_for_button()
+    #
+    # Your code goes here!
+    #
+
+main()
